d21870d45cd94975005599680759cc011256f855
zzzeelloo <102261049+zzzeelloo@users.noreply.github.com>
2024-12-06 17:26:22 +0100

    Update get_next_line.c

--- z42/gnl/get_next_line.c
+++ z42/gnl/get_next_line.c
@@ -3,63 +3,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// void ft_putstr(char *str)
-// {
-// 	size_t i;
+#define BUFFER_SIZE		5
 
-// 	i = 0;
-// 	while(str)
-// 	{
-// 		write(1, &str[i], 1);
-// 		i++;
-// 	}
-// }
 
 int ft_strlen(char *str)
 {
-	int i = 0;
+	int i;
 
+	if (!str)
+		return (0);
+	i = 0;
 	while (str[i])
 		i++;
 	return (i);
 }
-char	*ft_strchr_mod(char *str)
-{
-	int	i;
 
-	i = 0;
-	while (str[i])
-	{
-		if (str[i] == '\n')
-			return (&str[i]);
-		i++;
-	}
-	return (NULL);
-}
-char *ft_strjoin(char *str, char *str1)
-{
-	int i;
-	int y;
-	char *tab;
-
-	i = 0;
-	y = 0;
-	tab = malloc(sizeof(char) * (ft_strlen(str)+ ft_strlen(str1)) + 1);
-	if (!tab)
-		return (NULL);
-	while (str[i])
-	{
-		tab[i] = str[i];
-		i++;
-	}
-	while (str1[y])
-	{
-		tab[i + y] = str1[y];
-		y++;
-	}
-	tab[i + y] = '\0';
-	return(tab);
-}
 int	check_end_line(char *str)
 {
 	int i;
@@ -73,21 +31,108 @@ int	check_end_line(char *str)
 	}
 	return (0);
 }
+
+char    *ft_strchr_mod(char *str)
+{
+    int    i;
+    int y;
+    char *tab;
+
+    i = 0;
+    y = 0;
+    while (str[i] != '\n') // compte ce qu'il y a avant \n
+    {
+        if (str[i] == '\0')
+            return (NULL);
+        i++;
+    }
+    if (str[i] == '\n' && str[i + 1] == '\0')
+        return (NULL);
+    tab = malloc(sizeof(char) * ((ft_strlen(str) + i) + 1)); // pour le soustraire ici
+    if (!tab)
+        return (NULL);
+    i++;
+    while (i < ft_strlen(str))
+    {
+        tab[y] = str[i];
+        y++;
+        i++;
+    }
+    tab[y] = '\0';
+    return (tab);
+}
+// char	*ft_strjoin(char *s1, char *s2)
+// {
+// 	size_t	i;
+// 	size_t	j;
+// 	char	*ptr;
+// 	size_t	k;
+
+// 	k = ft_strlen(s1);
+// 	i = 0;
+// 	j = 0;
+// 	ptr = malloc(sizeof(char) * (k + ft_strlen(s2) + 1));
+// 	if (!ptr)
+// 		return (NULL);
+// 	while (s1 && i < k)
+// 	{
+// 		ptr[i] = s1[i];
+// 		i++;
+// 	}
+// 	while (s2 && i < ft_strlen(s2) + k)
+// 		ptr[i++] = s2[j++];
+// 	ptr[i] = '\0';
+// 	free(s1);
+// 	return (ptr);
+// }
+char *ft_strjoin(char *str, char *str1)
+{
+	int i;
+	int y;
+	char *tab;
+
+	i = 0;
+	y = 0;
+	tab = malloc(sizeof(char) * (ft_strlen(str)+ ft_strlen(str1)) + 1);
+	if (!tab)
+		return (NULL);
+	while (str[i] && i < ft_strlen(str))
+	{
+		tab[i] = str[i];
+		i++;
+	}
+	while (str1[y] && (i + y) < (ft_strlen(str1) + ft_strlen(str)))
+	{
+		tab[i + y] = str1[y];
+		y++;
+	}
+	tab[i + y] = '\0';
+	if (str)
+		free(str);
+	return(tab);
+}
+
 char *get_line(char *str, int fd)
 {
 	char *read_buff;
 	int r;
 
-	read_buff = malloc(sizeof(char) * 5 + 1);//buffer_size a la place de 5
-	if (!read_buff)
-		return (NULL);
-	while (1 || r == 0)
+	read_buff = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	// if (!read_buff)
+	// 	return (NULL);
+	while (1)
 	{
-		r = read(fd, read_buff, 5);//buffer_size a la place de 5
+		r = read(fd, read_buff, BUFFER_SIZE);
+		if (r == 0)
+			break ;
+		if (r == -1) //si erreur de read
+		{
+			free(str);
+            free(read_buff);
+            return NULL;
+        }
 		read_buff[r] = '\0';
 		str = ft_strjoin(str, read_buff);
-		if (!str)
-			return (NULL);
 		if (check_end_line(str) == 1)
 			break;
 	}
@@ -99,32 +144,44 @@ char *get_the_line(char *line)
 	int i;
 	char *real_line;
 
+	if (!line)
+		return(NULL);
 	i = 0;
-	while (line[i] || line[i] == '\n')
+	while (line[i] && line[i] != '\n')
 		i++;
-	real_line = malloc(sizeof(char) * (i + 1));
+	real_line = malloc(sizeof(char) * (i + 2));// \n et \0
 	if (!real_line)
 		return (NULL);
 	i = 0;
-	while (line[i] || line[i] == '\n')
+	while (line[i] && line[i] != '\n')
 	{
 		real_line[i] = line[i];
 		i++;
 	}
-	// real_line[i] = '\n';
+	if (line[i] == '\n')
+	{
+		real_line[i] = '\n';
+		i++;
+	}
 	real_line[i] = '\0';
 	return (real_line);
 }
 char *get_next_line(int fd)
 {
 	// char *tab;
-	char *next_line;
 	static char *stash;
+	char *next_line;
+	char *odd_stash;
 
+	if (fd == -1)
+		return (NULL);
 	stash = get_line(stash, fd);
+	odd_stash = stash;
 	next_line = get_the_line(stash);
+	if (!next_line)
+		return(NULL);
 	stash = ft_strchr_mod(stash);
-	free(stash);
+	free(odd_stash);
 	return(next_line);
 }
 
